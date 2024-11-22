0235ad47fe160ff75b28a6c066919462674faa39
Veenakrishna10 <veenapb55@gmail.com>
2024-11-22 12:14:02 +0530

    Bubblesort changes

--- java_learning/src/o2_number_programs/Bubblesort.java
+++ java_learning/src/o2_number_programs/Bubblesort.java
@@ -4,8 +4,25 @@ public class Bubblesort {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Bubble sort in java");
-
+		int a []= {5,8,4,3,1};
+		//System.out.println("a.length");
+		for(int i=1; i<=a.length; i++)
+		{
+			for(int j=0; j<=a.length-1-i; j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					int t = a[j];
+					a[j]= a[j+1];
+					a[j+1] = t;
+				}
+			}
+		}
+System.out.println("sorted array");
+for(int i=0; i<=a.length;i++)
+{
+	System.out.print(a[i] + " ");
+}
 	}
 
 }
