dbf4f85b8c1e8a92b59c589c3736c95c3eab5d95
Veenakrishna10 <veenapb55@gmail.com>
2024-11-22 11:16:59 +0530

    bubble sort altration

--- java_learning/src/o2_number_programs/Bubblesort.java
+++ java_learning/src/o2_number_programs/Bubblesort.java
@@ -3,8 +3,8 @@ package o2_number_programs;
 public class Bubblesort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Bubble sort");
+		
+		System.out.println("Bubble sort in java");
 
 	}
 
